bfeb7f01a00578dec3ae9eb6c433c1ca6e17926f
Josh Wright <joshwrightaudio@gmail.com>
2021-04-21 20:39:11 -0400

    psuedo code

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      {/* SIDEBAR */}
+
+      {/* feed */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ twitter-clone/src/Sidebar.js
@@ -0,0 +1,20 @@
+import react from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* TWITTER ICON */}
+      <TwitterIcon />
+      {/* SIDEBAR 1 */}
+      {/* SIDEBAR 2 */}
+      {/* SIDEBAR 3 */}
+      {/* SIDEBAR 4 */}
+      {/* SIDEBAR 5 */}
+      {/* SIDEBAR 6 */}
+    </div>
+  );
+}
+
+export default App;
